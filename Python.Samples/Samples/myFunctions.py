850687580cc7264a2464fb0b002caf7e165fa184
hong.pham-huu <hong.pham-huu@wize-solutions.com>
2020-12-07 11:04:23 +0700

    Create function mySplit

--- Python.Samples/Samples/myFunctions.py
+++ Python.Samples/Samples/myFunctions.py
@@ -6,26 +6,46 @@ def readint(prompt, min, max):
         else:
             return int(strVal)
 
-def mySplit(str, spliter = " "):
+def mySplit(str):
+    # return empty if only spaces
+    if str.isspace() or len(str)==0:
+        return []
+
+    str = str.lstrip().rstrip()
+    spliter = " "
     sPos = 0
     list = []
-    while sPos < len(str) - 1 :
+
+
+    ePos = str.find(spliter, sPos)
+    # if does not find any space return the list with on an item
+    if ePos == -1:
+        list.append(str)
+        return list
+
+    while True:
+        # if start with space
         ePos = str.find(spliter, sPos)
+        # if the next word end with "\r\n"
+        
         if ePos == -1:
-            return str
-
-        if ePos == 0:
-            return []
-
-        word = str[sPos : ePos]
-        list.append(word)
+            epos = str.find("\r\n", sPos)
+            word = str[sPos:]
+        else:
+            word = str[sPos : ePos]
+        if word != "":
+            list.append(word)
         sPos += len(word) + 1
+        if sPos > len(str):
+            break
 
     return list
 
-def myPrint(arr):
+def printStringArray(arr):
+    if len(arr) == 0:
+        return "[]"
     result = "["
     for item in arr:
-        result += item + ", "
+        result += "'" + item + "', "
 
-    return result[0:-1] + "]"
+    return result[0:-2] + "]"
--- Python.Samples/tests/test_myFunctions.py
+++ Python.Samples/tests/test_myFunctions.py
@@ -5,12 +5,11 @@ import Samples.myFunctions as my
 
 class Test_MyFunctions(unittest.TestCase):
     def test_mySplit(self):
-        print(my.myPrint(my.mySplit("To be or not to be, that is the question")))
-        #self.assertEqual(print(list(my.mySplit("To be or not to be, that is the question"))),"['To', 'be', 'or', 'not', 'to', 'be,', 'that', 'is', 'the', 'question']")
-        #self.assertEqual(print(list(my.mySplit("To be or not to be,that is the question"))),"['To', 'be', 'or', 'not', 'to', 'be,that', 'is', 'the', 'question']")
-        #self.assertEqual(print(list(my.mySplit("   "))),"[]")
-        #self.assertEqual(print(list(my.mySplit(" abc "))),"['abc']")
-        #self.assertEqual(print(list(my.mySplit(""))),"[]")
+        self.assertEqual(my.printStringArray(my.mySplit("To be or not to be, that is the question")),"['To', 'be', 'or', 'not', 'to', 'be,', 'that', 'is', 'the', 'question']")
+        self.assertEqual(my.printStringArray(my.mySplit("To be or not to be,that is the question")),"['To', 'be', 'or', 'not', 'to', 'be,that', 'is', 'the', 'question']")
+        self.assertEqual(my.printStringArray(my.mySplit("   ")),"[]")
+        self.assertEqual(my.printStringArray(my.mySplit(" abc ")),"['abc']")
+        self.assertEqual(my.printStringArray(my.mySplit("")),"[]")
 
 if __name__ == '__main__':
     unittest.main()
